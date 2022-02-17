5a9fc90bc24c6310ff84a42e3cc9b01c08827ef1
Raghav <83136390+Raghav-Bell@users.noreply.github.com>
2022-02-17 16:25:32 +0530

    Add files via upload

---
+++ koch_coll.py
@@ -0,0 +1,33 @@
+import turtle
+
+
+z=turtle.Turtle()
+#Koch method will draw koch curve .
+def koch(t,l):
+ angle1=60
+ angle=120
+ if l>3 :
+  koch(t,l/3)
+  t.lt(angle1)
+  koch(t,l/3)
+  t.rt(angle)
+  koch(t,l/3)
+  t.lt(angle1)
+  koch(t,l/3)
+ else :
+  t.fd(l)
+#Taking input.
+n=int(input("Enter a natural number")) 
+#Collataz Conjecture for generating random number (More Fun!).      
+while int(n)!=1:
+ if(n%2==0):
+  z.lt(60)
+  n=n/2
+  koch(z,n)
+            
+ else:
+  z.lt(120)
+  n=(3*n+1)/2
+  koch(z,n)
+#End
+turtle.mainloop()
